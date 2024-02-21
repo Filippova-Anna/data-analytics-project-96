2ce08a8fbf4d92c485f3050c86e245d4dc8ec980
Filippova-Anna <115940569+Filippova-Anna@users.noreply.github.com>
2024-02-22 02:33:14 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,14 +1,14 @@
 with tab as (
     select
         s.visitor_id,
-        closing_reason,
+        l.closing_reason,
         s."source" as utm_source,
         s.medium as utm_medium,
         s.campaign as utm_campaign,
-        lead_id,
+        l.lead_id,
         l.amount,
-        created_at,
-        status_id,
+        l.created_at,
+        l.status_id,
         date(s.visit_date) as visit_date,
         row_number()
             over (partition by s.visitor_id order by s.visit_date desc)
@@ -36,7 +36,11 @@ tab_2 as (
         utm_campaign,
         sum(daily_spent) as daily_spent
     from vk_ads
-    group by 1, 2, 3, 4
+    group by
+        campaign_date::date,
+        utm_source,
+        utm_medium,
+        utm_campaign
     union all
     select
         campaign_date::date,
@@ -45,19 +49,23 @@ tab_2 as (
         utm_campaign,
         sum(daily_spent) as daily_spent
     from ya_ads
-    group by 1, 2, 3, 4
+    group by
+        campaign_date::date,
+        utm_source,
+        utm_medium,
+        utm_campaign
 )
 
 select
-    visit_date,
-    count(*) as visitors_count,
+    a.visit_date,
     a.utm_source,
     a.utm_medium,
     a.utm_campaign,
-    daily_spent as total_cost,
-    count(*) filter (where lead_id is not null) as leads_count,
-    count(*) filter (where status_id = 142) as purchases_count,
-    coalesce(sum(amount) filter (where status_id = 142), 0) as revenue
+    b.daily_spent as total_cost,
+    count(*) as visitors_count,
+    count(*) filter (where a.lead_id is not null) as leads_count,
+    count(*) filter (where a.status_id = 142) as purchases_count,
+    coalesce(sum(a.amount) filter (where a.status_id = 142), 0) as revenue
 from tab as a
 left join tab_2 as b
     on
@@ -65,14 +73,19 @@ left join tab_2 as b
         and a.utm_medium = b.utm_medium
         and a.utm_campaign = b.utm_campaign
         and a.visit_date = b.campaign_date
-where rn = 1
-group by 1, 3, 4, 5, 6
+where a.rn = 1
+group by
+    a.visit_date,
+    a.utm_source,
+    a.utm_medium,
+    a.utm_campaign,
+    b.daily_spent
 order by
     revenue desc nulls last,
-    visit_date asc,
+    a.visit_date asc,
     visitors_count desc,
-    utm_source asc,
-    utm_medium asc,
-    utm_campaign asc
+    a.utm_source asc,
+    a.utm_medium asc,
+    a.utm_campaign asc
 limit 15;
 --топ 15: витрина с расходами на рекламу по модели атрибуции Last Paid Click.
