f95760188ce506c6f872b17cf83407ae0fb3a171
Filippova-Anna <115940569+Filippova-Anna@users.noreply.github.com>
2024-02-25 00:59:27 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,3 +1,4 @@
+-- Рассчет основных метрик (таблица)
 SELECT
     visit_date,
     utm_source,
@@ -8,104 +9,80 @@ SELECT
     ROUND(total_cost / purchases_count, 2) AS cppu,
     ROUND((revenue - total_cost) / total_cost * 100, 2) AS roi
 FROM agg_last_click_all;
---Рассчет основных метрик (таблица)
 
+-- Средняя посещаемость по каналам
 SELECT
-    visit_date AS visit_date,
-    utm_source AS utm_source,
-    AVG(visitors_count) AS "AVG(visitors_count)"
+    visit_date,
+    utm_source,
+    AVG(visitors_count) AS avg_visitors_count
 FROM main.agg_last_click_all
 GROUP BY
-	visit_date,
+    visit_date,
     utm_source
-ORDER BY AVG(visitors_count) DESC
-LIMIT 10000
-OFFSET 0;
---Средняя посещаемость по каналам
+ORDER BY AVG(visitors_count) DESC;
 
+-- Средняя посещаемость за июнь
 SELECT
-	visit_date AS visit_date,
-    AVG(visitors_count) AS "AVG(visitors_count)"
+    visit_date,
+    AVG(visitors_count) AS avg_visitors_count
 FROM main.agg_last_click_all
+WHERE EXTRACT(MONTH FROM visit_date) = 6
 GROUP BY visit_date
-ORDER BY "AVG(visitors_count)" DESC
-LIMIT 5000
-OFFSET 0;
---Средняя посещаемость за июнь
+ORDER BY AVG(visitors_count) DESC;
 
+-- Затраты на рекламу по источнику
 SELECT
-	utm_source AS utm_source,
-    sum(total_cost) AS "SUM(total_cost)"
+    utm_source,
+    SUM(total_cost) AS sum_total_cost
 FROM main.agg_last_click_all
-WHERE utm_source IN ('yandex',
-                     'vk')
+WHERE utm_source IN ('yandex', 'vk')
 GROUP BY utm_source
-ORDER BY "SUM(total_cost)" DESC
-LIMIT 10000
-OFFSET 0;
---Затраты на рекламу по источнику
+ORDER BY sum_total_cost DESC;
 
+-- Количество лидов, приведенных из каналов
 SELECT
-	utm_source AS utm_source,
-    sum(leads_count) AS "SUM(leads_count)"
+    utm_source,
+    SUM(leads_count) AS sum_leads_count
 FROM main.agg_last_click_all
 GROUP BY utm_source
-ORDER BY "SUM(leads_count)" DESC
-LIMIT 100
-OFFSET 0;
---Количество лидо приведенных из каналов
+ORDER BY sum_leads_count DESC;
 
+-- Цена привлечения одного потенциального клиента с рекламной кампании
 SELECT
-	utm_source AS utm_source,
-    AVG(cpl) AS "AVG(cpl)"
-FROM
-  (SELECT
-		visit_date,
+    utm_source,
+    AVG(cpl) AS avg_cpl
+FROM (
+    SELECT
         utm_source,
-        utm_medium,
-        utm_campaign,
-        total_cost / visitors_count cpu,
-        total_cost / leads_count cpl,
-        total_cost / purchases_count cppu,
-        (revenue - total_cost) / total_cost * 100 roi
-   from 'agg_last_click_all') AS virtual_table
+        total_cost / leads_count AS cpl
+    FROM main.agg_last_click_all
+) AS virtual_table
 GROUP BY utm_source
-ORDER BY "AVG(cpl)" DESC
-LIMIT 100
-OFFSET 0;
---Цена привлечения одного потенциального клиента с конкретной рекламной кампании
+ORDER BY avg_cpl DESC;
 
+-- Среднее значение ROI по источнику и компании
 SELECT
-	utm_source AS utm_source,
-    AVG(roi) AS "AVG(roi)"
-FROM
-  (SELECT
-		visit_date,
+    utm_source,
+    AVG(roi) AS avg_roi
+FROM (
+    SELECT
         utm_source,
-        utm_medium,
-        utm_campaign,
-        otal_cost / visitors_count cpu,
-        total_cost / leads_count cpl,
-        total_cost / purchases_count cppu,
-        (revenue - total_cost) / total_cost * 100 roi
-   from 'agg_last_click_all') AS virtual_table
+        (revenue - total_cost) / total_cost * 100 AS roi
+    FROM main.agg_last_click_all
+) AS virtual_table
 GROUP BY utm_source
-ORDER BY "AVG(roi)" DESC
-LIMIT 10000
-OFFSET 0;
---Среднее значение ROI по источнику и компании
+ORDER BY avg_roi DESC;
 
-with tab AS (
-	SELECT
-		s.visitor_id,
-		min(s.visit_date::date) AS first_visit,
-		l.created_at::date AS lead_first
-	FROM sessions s
-	JOIN leads l ON l.visitor_id = s.visitor_id
-	GROUP BY 1, 3
-	ORDER BY 1
+-- Среднее время от визита до становления лидом
+WITH tab AS (
+    SELECT
+        sessions.visitor_id,
+        leads.created_at::date AS lead_first,
+        MIN(sessions.visit_date::date) AS first_visit
+    FROM sessions
+    INNER JOIN leads ON sessions.visitor_id = leads.visitor_id
+    GROUP BY sessions.visitor_id, leads.created_at
 )
-SELECT
-	avg(lead_first - first_visit)
+
+SELECT AVG(lead_first - first_visit) AS average_time_to_lead
 FROM tab;
---Среднее время от визита до становления лидом
